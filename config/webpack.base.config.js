b7f4fc996fa1fb8a6a6a205cf9aaf86f4a476b69
Daniel Mendez <Daniel.Mendez@ey.com>
2018-11-06 09:34:37 -0600

    added components Landing, Login , NavBar, SignUp

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -23,7 +23,17 @@ module.exports = env => {
                 'css-loader',
                 'sass-loader'
               ]
-            }
+            },
+            {
+              test: /\.(png|jp(e*)g|svg|gif)$/,  
+              use: [{
+                  loader: 'url-loader',
+                  options: { 
+                      limit: 8000, // Convert images < 8kb to base64 strings
+                      name: 'images/[hash]-[name].[ext]'
+                  } 
+              }]
+          }
           ]
         },
         plugins: [
